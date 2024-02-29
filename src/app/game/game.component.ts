9c19ca219390260aeaf132ddadb0730fef288eb4
Ghulammustafa Kakar <mustafakakar01@yahoo.com>
2024-02-29 15:58:23 +0100

    added played-cards deck

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Game } from '../../models/game';
 
 @Component({
   selector: 'app-game',
@@ -10,8 +11,30 @@ import { Component } from '@angular/core';
 })
 export class GameComponent {
   pickCardAnimation = false;
+  currentCard = '';
+  game: Game;
+
+  constructor() {
+    this.game = new Game();
+    this.newGame();
+  }
+
+  newGame() {
+    console.log(this.game)
+  }
 
   takeCard() {
-    this.pickCardAnimation = true;
+    if (!this.pickCardAnimation) {
+      this.currentCard = this.game.stack.pop() ?? '';
+      this.pickCardAnimation = true;
+
+      console.log('New card:', this.currentCard)
+      console.log(this.game)
+    }
+
+    setTimeout(() => {
+      this.game.playedCards.push(this.currentCard);
+      this.pickCardAnimation = false;
+    }, 1000);
   }
 }
