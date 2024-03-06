1a59b1414f32bf672ce70518525d368691e65c43
Ghulammustafa Kakar <mustafakakar01@yahoo.com>
2024-03-06 20:53:33 +0100

    forward to new game

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, inject } from '@angular/core';
+import { Component, Injectable, inject } from '@angular/core';
 import { Game } from '../../models/game';
 import { PlayerComponent } from '../player/player.component';
 import { MatButtonModule } from '@angular/material/button';
@@ -7,8 +7,10 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 import { DialogAddPlayerComponent } from '../dialog-add-player/dialog-add-player.component';
 import { GameInfoComponent } from '../game-info/game-info.component';
-import { Firestore, collection, collectionData, addDoc } from '@angular/fire/firestore';
+import { Firestore, collection, collectionData, addDoc, getDoc, doc } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
+import { ActivatedRoute } from '@angular/router';
+import { StartScreenComponent } from '../start-screen/start-screen.component';
 
 @Component({
   selector: 'app-game',
@@ -22,21 +24,44 @@ export class GameComponent {
   currentCard = '';
   game: Game;
   firestore: Firestore = inject(Firestore);
-  items$: Observable<any[]>;
+  // items$: Observable<any[]>;
+  // items;
 
-  constructor(public dialog: MatDialog) {
+
+  constructor(private route: ActivatedRoute, public dialog: MatDialog) {
     this.game = new Game();
     this.newGame();
-    const aCollection = this.getGamesRef();
-    this.items$ = collectionData(aCollection);
-    this.items$.subscribe((game) => {
-      console.log('Game update', game);
+
+    // this.items$ = collectionData(this.getGamesRef());
+    // this.items = this.items$.subscribe((list) => {
+    //   list.forEach(element => {
+    //     console.log(element);
+    //   });
+    // });
+    this.route.params.subscribe((params) => {
+      this.initCurrentGame(params['id']);
     });
   }
 
   async newGame() {
     console.log(this.game);
-    await addDoc(this.getGamesRef(), this.game.toJson());
+
+  }
+
+
+  async initCurrentGame(id: string) {
+    const docRef = doc(this.firestore, "games", id);
+    const docSnap = await getDoc(docRef);
+    if (docSnap.exists()) {
+      const data = docSnap.data();
+      this.game.currentPlayer = data['currentPlayer'];
+      this.game.playedCards = data['playedCards'];
+      this.game.players = data['players'];
+      this.game.stack = data['stack'];
+    } else {
+      // docSnap.data() will be undefined in this case
+      console.log("No such document!");
+    }
   }
 
   getGamesRef() {
--- src/app/start-screen/start-screen.component.ts
+++ src/app/start-screen/start-screen.component.ts
@@ -1,5 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { Router } from '@angular/router';
+import { Firestore, collection, collectionData, addDoc, getDoc, doc } from '@angular/fire/firestore';
+import { Game } from '../../models/game';
+import { GameComponent } from '../game/game.component';
+
 
 @Component({
   selector: 'app-start-screen',
@@ -9,13 +13,22 @@ import { Router } from '@angular/router';
   styleUrl: './start-screen.component.scss'
 })
 export class StartScreenComponent {
+  firestore: Firestore = inject(Firestore);
 
   constructor(private router: Router) {
 
   }
 
-  newGame() {
+  async newGame() {
     // Start game
-    this.router.navigateByUrl('/game');
+    let game = new Game();
+    await addDoc(this.getGamesRef(), game.toJson()).then((gameInfo:any) => {
+      this.router.navigateByUrl('/game/' + gameInfo['_key']['path']['segments'][1]);
+    });
+
+  }
+
+  getGamesRef() {
+    return collection(this.firestore, 'games');
   }
 }
